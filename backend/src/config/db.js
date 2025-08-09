8d6b6bc00301995b4c33c406d8a6462949484c23
rizkirenden <rizkirenden@gmail.com>
2025-08-09 09:14:22 +0800

    backend

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,11 +1,20 @@
-require("dotenv").config();
-const mysql = require("mysql2/promise");
+import pkg from "pg";
+import dotenv from "dotenv";
+dotenv.config();
 
-const pool = mysql.createPool({
+const { Pool } = pkg;
+
+const pool = new Pool({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
 });
 
-module.exports = pool;
+pool
+  .connect()
+  .then(() => console.log("✅ PostgreSQL Connected"))
+  .catch((err) => console.error("❌ Connection error", err.stack));
+
+export default pool;
---
+++ backend/src/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const pool = require("./src/config/db"); // koneksi PG
+
+dotenv.config();
+
+const app = express();
+const port = 3000;
+const routes = require("./src/routes/routes");
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/uploads", express.static("public/uploads"));
+app.use("/api", routes);
+
+app.listen(port, () => {
+  console.log(`🚀 Server berjalan di http://localhost:${port}`);
+});
